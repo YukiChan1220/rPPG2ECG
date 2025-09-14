b2bf986ee504b1ff6b182455c42571638df87930
Cheng Songqin <2140499180@qq.com>
2025-09-14 15:55:08 +0800

    Updated data review and cleaning process

---
+++ train/dataset.py
@@ -0,0 +1,19 @@
+import torch
+from torch.utils.data import Dataset
+
+class RPPG2ECGDataset(Dataset):
+    def __init__(self, X_rppg_list, Y_ecg_list, window_samples):
+        # X_rppg_list, Y_ecg_list: list of 1D numpy arrays, already same fs & preprocessed
+        self.windows = []
+        for r, e in zip(X_rppg_list, Y_ecg_list):
+            n = len(r)
+            step = window_samples // 2
+            for i in range(0, n-window_samples+1, step):
+                xr = r[i:i+window_samples]
+                ye = e[i:i+window_samples]
+                self.windows.append((xr.astype('float32'), ye.astype('float32')))
+
+    def __len__(self): return len(self.windows)
+    def __getitem__(self, idx):
+        xr, ye = self.windows[idx]
+        return torch.from_numpy(xr).unsqueeze(0), torch.from_numpy(ye).unsqueeze(0)  # (1,L)
